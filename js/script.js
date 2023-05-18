ae87b4901976620f9d1bd5d09bc71743c13dd0e1
ASTIK <astik.ytt@gmail.com>
2023-05-18 20:43:05 +0200

    Add responsiveness to header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 function toggleLanguageMenu() {
 	document.querySelector(".header__language-content").classList.toggle("header__language-content_open");
 }
+
+function burgerMenu(value) {
+	let menu = document.querySelector(".header__menu");
+	let openClass = "header__menu_open";
+
+	if (value == "open") {
+		menu.classList.add(openClass);
+	} else {
+		menu.classList.remove(openClass);
+	}
+
+	if (document.querySelector(".header__language-content_open")) toggleLanguageMenu();
+}
